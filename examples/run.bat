92744b3fee427df9ea2d558d02ebc80ab820a138
xrisaD <xrdikonimaki@hotmail.com>
2022-03-04 19:52:21 +0100

    Add script which creates all the replicas

---
+++ examples/run.bat
@@ -0,0 +1,4 @@
+start cargo run --example gouged -- --id 1 --peers 2 3
+start cargo run --example gouged -- --id 2 --peers 1 3
+start cargo run --example gouged -- --id 3 --peers 1 2
+start cargo run --example gouge
