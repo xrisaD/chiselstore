4843b113c16e1d0310aebcf94b77fb8879514429
xrisaD <xrdikonimaki@hotmail.com>
2022-03-25 11:58:02 +0100

    add examples back

--- examples/example.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE contacts ( contact_id INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, phone TEXT NOT NULL UNIQUE );
-
-CREATE TABLE idontknow (id INTEGER);
-INSERT INTO idontknow (id) VALUES (0);
-
-SELECT * FROM idontknow;
